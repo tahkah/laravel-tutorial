4f4d149a36fc63eda6d92ae0aece6622bd8ef9a7
takahira <takahira>
2023-09-14 06:27:46 +0000

    投稿を新規で登録するページを作成しバリデーションを設定しました。

--- project/resources/views/welcome.blade.php
+++ project/resources/views/welcome.blade.php
@@ -1,6 +1,19 @@
 <html>
-    <header>Laravel</header>
-    <body>
-        <h1>Hello world!</h1>
-    </body>
+    <header>
+        Laravel
+    </header>
+ <body>
+    
+    <form action="#" method="post">
+        <p>タイトル<br>
+            <input type="text" name="message" required /></p>
+            <p>内容<br>
+            <input type="text" name="message"></p>
+            <p><input type="submit" value="登録"> <input type="reset" value="キャンセル"></p>
+            
+    
+    </form>
+        
+　</body>
 </html>
+
